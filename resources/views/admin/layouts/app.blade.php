ada6b61f805885d138ba539fc688602bbeb920cc
xerataolo <0306221071@caothang.edu.vn>
2025-01-07 22:38:05 +0700

    thach ne

---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha384-k6RqeWeci5ZR/Lv4MR0sA0FfDOM/eGbBzqDf6e5hRYfjFEYNB5F0O7NB4cM7xpc1" crossorigin="anonymous">
+</head>
+<body>
+    <div class="dashboard-container">
+        <!-- Header -->
+        <header>
+            <h1>Admin Dashboard</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Dashboard</a></li>
+                    <li><a href="#">Users</a></li>
+                    <li><a href="#">Orders</a></li>
+                    <li><a href="#">Directory</a></li>
+                    <li><a href="{{ route('products.index') }}">Products</a></li>
+                    <li><a href="{{ route('comments.index') }}">Comment</a></li>
+                    <li><a href="{{ route('lienhe.index') }}">Contact</a></li>    
+                    <li><a href="#">Settings</a></li>
+                </ul>
+            </nav>
+        </header>
+        <div class="main-content">
+            @yield('content1')
+            @yield('comment')
+            @yield('lienhe')
+            @yield('content2')
+            @yield('content3')
+        </div>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,33 @@ Route::post('/rates/add', [HomeController::class, 'AddRates'])->name('rates.add'
 //yeuthich
 Route::post('/favorite/add', [HomeController::class, 'AddFavorite'])->name('favorite.add');
 
+//comment
+// Danh sách bình luận
+Route::get('admin/comments', [CommentController::class, 'index'])->name('comments.index');
+// Xoá bình luận
+Route::delete('admin/comments/{id}', [CommentController::class, 'destroy'])->name('comments.destroy');
 
+
+//lienhe
+Route::get('admin/lienhe', [LienHeController::class, 'index'])->name('lienhe.index');
+Route::put('admin/lienhe/{id}', [LienHeController::class, 'update'])->name('lienhe.update');
+Route::delete('admin/lienhe/{id}', [LienHeController::class, 'destroy'])->name('lienhe.destroy');
+
+//product
+// Danh sách sản phẩm + tìm kiếm
+Route::get('admin/products', [ProductController::class, 'index'])->name('products.index');
+
+// Form thêm
+Route::get('admin/products/create', [ProductController::class, 'create'])->name('products.create');
+// Lưu
+Route::post('admin/products', [ProductController::class, 'store'])->name('products.store');
+
+// Form sửa
+Route::get('admin/products/{id}/edit', [ProductController::class, 'edit'])->name('products.edit');
+// Cập nhật
+Route::put('admin/products/{id}', [ProductController::class, 'update'])->name('products.update');
+
+// Xóa
+Route::delete('admin/products/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
 
 
